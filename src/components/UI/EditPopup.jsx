8b0801e4aa1c57989a0b51f619b3ad1f8b90716b
anasahtsham <mh.anasahtsham@gmail.com>
2023-12-21 13:09:46 +0500

    reload on edit

--- src/components/UI/EditPopup.jsx
+++ src/components/UI/EditPopup.jsx
@@ -1,10 +1,12 @@
 import React, { useState } from "react";
 import { BASE_URL } from "../../utils/constants";
 import axios from "axios";
+// import { useHistory } from "react-router-dom"; // to navigate to dashboard
 import "../../assets/css/EditPopup.css";
 
 export default function EditPopup(props) {
   const [popup, setPopup] = useState(false);
+  // const history = useHistory();
 
   const togglePopup = () => {
     setPopup(!popup);
@@ -28,8 +30,8 @@ export default function EditPopup(props) {
         phone: formData.get("phone"),
       });
       if (response?.data) {
-        alert("Edited Successfully.");
         setPopup(false);
+        window.location.reload();
       }
     } catch (error) {
       alert("Error: " + error);
