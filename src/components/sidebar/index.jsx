beefb08aad65e073e65378f148612cff4fea99e2
Siamax <ibrahimahtsham2002@gmail.com>
2024-04-12 22:02:11 +0500

    stylings made responsive

--- src/components/sidebar/index.jsx
+++ src/components/sidebar/index.jsx
@@ -1,12 +1,14 @@
 import { useContext } from "react";
 import { SidebarContext } from "../../utils/SidebarContext";
+import { ThemeContext } from "../../utils/ThemeContext";
 import "./Sidebar.css"; // Ensure you're importing the CSS file
 
 const Sidebar = () => {
+  const { theme } = useContext(ThemeContext);
   const { isOpen } = useContext(SidebarContext);
 
   return (
-    <div className={`sidebar ${isOpen ? "open" : ""}`}>
+    <div className={`sidebar ${isOpen ? "open" : ""} ${theme}`}>
       <p>sidebar</p>
     </div>
   );
