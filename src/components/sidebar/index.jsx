18ac64b298adeb3ace02cbc16620d84015c8cd17
Siamax <ibrahimahtsham2002@gmail.com>
2024-04-13 09:11:54 +0500

    stylings changed for sidebar, and dashboard

--- src/components/sidebar/index.jsx
+++ src/components/sidebar/index.jsx
@@ -32,10 +32,46 @@ const Sidebar = () => {
           isActive={activeButton === "Button 3"}
           onClick={() => setActiveButton("Button 3")}
         />
+        <SidebarButton
+          image={icons.sunIcon}
+          title="Button 4"
+          isActive={activeButton === "Button 4"}
+          onClick={() => setActiveButton("Button 4")}
+        />
+        <SidebarButton
+          image={icons.sunIcon}
+          title="Button 5"
+          isActive={activeButton === "Button 5"}
+          onClick={() => setActiveButton("Button 5")}
+        />
+        <SidebarButton
+          image={icons.sunIcon}
+          title="Button 6"
+          isActive={activeButton === "Button 6"}
+          onClick={() => setActiveButton("Button 6")}
+        />
+        <SidebarButton
+          image={icons.sunIcon}
+          title="Button 7"
+          isActive={activeButton === "Button 7"}
+          onClick={() => setActiveButton("Button 7")}
+        />
+        <SidebarButton
+          image={icons.sunIcon}
+          title="Button 8"
+          isActive={activeButton === "Button 8"}
+          onClick={() => setActiveButton("Button 8")}
+        />
+        <SidebarButton
+          image={icons.sunIcon}
+          title="Button 9"
+          isActive={activeButton === "Button 9"}
+          onClick={() => setActiveButton("Button 9")}
+        />
       </div>
       <div className="copyright-container">
         <img
-          style={{ width: isOpen ? "auto" : "30px" }}
+          style={{ width: isOpen ? "" : "30px" }}
           className="copyright-logo"
           src={icons.swiftRentLogoWhiteSVG}
           alt="swift rent logo"
