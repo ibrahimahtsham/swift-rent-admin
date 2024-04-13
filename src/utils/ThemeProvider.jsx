26e41608a0f0319e0875dd02c6f4692fdb41ecc8
Siamax <ibrahimahtsham2002@gmail.com>
2024-04-13 08:57:33 +0500

    updated stylings and storing theme in cookies now

--- src/utils/ThemeProvider.jsx
+++ src/utils/ThemeProvider.jsx
@@ -1,11 +1,22 @@
+import Cookies from "js-cookie";
 import { useState } from "react";
 import { ThemeContext } from "./ThemeContext";
 
 export const ThemeProvider = ({ children }) => {
-  const [theme, setTheme] = useState("light");
+  const [theme, setTheme] = useState(() => {
+    // Try to get the theme from a cookie
+    const cookieTheme = Cookies.get("theme");
+    // If the cookie exists, use its value as the initial theme, otherwise default to "light"
+    return cookieTheme ? cookieTheme : "light";
+  });
 
   const toggleTheme = () => {
-    setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
+    setTheme((prevTheme) => {
+      const newTheme = prevTheme === "light" ? "dark" : "light";
+      // When the theme is toggled, update the cookie
+      Cookies.set("theme", newTheme);
+      return newTheme;
+    });
   };
 
   return (
