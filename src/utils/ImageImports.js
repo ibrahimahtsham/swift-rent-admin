7b94686dc2a3aa5f0cab962cdf3d27117355b09f
Siamax <ibrahimahtsham2002@gmail.com>
2024-04-13 08:35:33 +0500

    removed unused image imports

--- src/utils/ImageImports.js
+++ src/utils/ImageImports.js
@@ -3,16 +3,10 @@ import moonIcon from "../assets/icons/moon.png";
 import sunIconWhite from "../assets/icons/sun-white.png";
 import sunIcon from "../assets/icons/sun.png";
 import swiftRentLogoBlackSVG from "../assets/icons/swift-rent-logo-black.svg";
-import swiftRentLogoColor from "../assets/icons/swift-rent-logo-color.png";
 import swiftRentLogoColorSVG from "../assets/icons/swift-rent-logo-colored.svg";
-import swiftRentLogoWhite from "../assets/icons/swift-rent-logo-white.png";
 import swiftRentLogoWhiteSVG from "../assets/icons/swift-rent-logo-white.svg";
-import swiftRentLogo from "../assets/icons/swift-rent-logo.png";
 
 export const icons = {
-  swiftRentLogoColor,
-  swiftRentLogoWhite,
-  swiftRentLogo,
   sunIcon,
   sunIconWhite,
   moonIcon,
