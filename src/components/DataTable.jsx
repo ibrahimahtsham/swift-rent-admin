5cd45aaa81b89ac5a1244d7a0ec0f9d8923a1722
Siamax <ibrahimahtsham2002@gmail.com>
2024-04-15 10:25:36 +0500

    removed disableRowSelectionOnClick in DataTable component

--- src/components/DataTable.jsx
+++ src/components/DataTable.jsx
@@ -33,7 +33,6 @@ const DataTable = (props) => {
             pagination: { paginationModel: { pageSize: 5 } },
           }}
           pageSizeOptions={[5, 10, 25]}
-          disableRowSelectionOnClick
         />
       </div>
     </ThemeProvider>
