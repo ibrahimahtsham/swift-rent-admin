26d5bce67e3d7029e150c8bd4d9d701705616057
Siamax <ibrahimahtsham2002@gmail.com>
2024-04-15 10:23:40 +0500

    validation updated for user edit

--- src/utils/validation/EditUserValidation.js
+++ src/utils/validation/EditUserValidation.js
@@ -1,10 +1,22 @@
 import * as Yup from "yup";
 
 export const editUserSchema = Yup.object().shape({
-  firstName: Yup.string().required("Required"),
-  lastName: Yup.string().required("Required"),
+  firstName: Yup.string()
+    .matches(/^\S*$/, "No spaces allowed")
+    .required("Required"),
+  lastName: Yup.string()
+    .matches(/^\S*$/, "No spaces allowed")
+    .required("Required"),
   dob: Yup.date().required("Required"),
-  phone: Yup.string().required("Required"),
-  email: Yup.string().email("Invalid email").required("Required"),
-  registeredOn: Yup.date().required("Required"),
+  phone: Yup.string()
+    .matches(
+      /^03\d{9}$/,
+      "Phone number must start with 03 and be 11 digits long"
+    )
+    .matches(/^\S*$/, "No spaces allowed")
+    .required("Required"),
+  email: Yup.string()
+    .email("Invalid email")
+    .matches(/^\S*$/, "No spaces allowed")
+    .required("Required"),
 });
