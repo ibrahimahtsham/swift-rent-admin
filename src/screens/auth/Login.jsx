e23bd1b25a063fd6d0bc4d0531a61578e3c1bd6e
anas <mh.anasahtsham@gmail.com>
2024-04-17 22:28:14 +0500

    Fix capitalization in import statement for loginValidation in Login.jsx

--- src/screens/auth/Login.jsx
+++ src/screens/auth/Login.jsx
@@ -2,7 +2,7 @@ import { useFormik } from "formik";
 import { useContext, useEffect } from "react";
 import { AuthContext } from "../../utils/AuthContext.jsx";
 import { icons } from "../../utils/ImageImports.js";
-import { loginValidation } from "../../utils/validation/loginValidation.js";
+import { loginValidation } from "../../utils/validation/LoginValidation.js";
 import { ThemeContext } from "./../../utils/ThemeContext.js";
 import "./Login.css";
 import LoginForm from "./form/LoginForm.jsx";
