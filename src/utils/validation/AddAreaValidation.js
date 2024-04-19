1ea2f6a02a73e80dfd95edc68067d83833f6567e
Siamax <ibrahimahtsham2002@gmail.com>
2024-04-19 18:10:16 +0500

    updated validation for add area

--- src/utils/validation/AddAreaValidation.js
+++ src/utils/validation/AddAreaValidation.js
@@ -3,6 +3,9 @@ import * as Yup from "yup";
 export const addAreaValidationSchema = Yup.object().shape({
   area: Yup.string()
     .required("Area is required")
-    .matches(/^\S*$/, "No white spaces are allowed"),
-  city: Yup.string().required("City is required"),
+    .matches(/^[^\s].*[^\s]$/, "No white spaces at the start or end")
+    .matches(/^(?!.*\s\s).*$/, "No double spaces"),
+  city: Yup.string()
+    .required("City is required")
+    .matches(/^\S*$/, "No white spaces"),
 });
