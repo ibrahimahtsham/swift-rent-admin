e92e76684c06c71840358cdcf0fe7733d3fb0afe
Siamax <ibrahimahtsham2002@gmail.com>
2024-04-19 18:18:18 +0500

    add city validation updated

--- src/utils/validation/AddCityValidation.js
+++ src/utils/validation/AddCityValidation.js
@@ -3,5 +3,5 @@ import * as Yup from "yup";
 export const addCityValidationSchema = Yup.object().shape({
   city: Yup.string()
     .required("City is required")
-    .matches(/^\S*$/, "No white spaces are allowed"),
+    .matches(/^\S*$/, "No white spaces"),
 });
