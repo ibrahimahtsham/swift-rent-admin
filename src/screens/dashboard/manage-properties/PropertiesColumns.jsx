c2533f933677c395b326cc19f961c25986da64e5
Siamax <ibrahimahtsham2002@gmail.com>
2024-05-10 18:02:45 +0500

    changed positionings for full address in property table

--- src/screens/dashboard/manage-properties/PropertiesColumns.jsx
+++ src/screens/dashboard/manage-properties/PropertiesColumns.jsx
@@ -4,6 +4,7 @@ export const getColumns = (setOpen, setEditingRowId, setEditingRowData) => [
     headerName: "Property ID",
     width: 150,
   },
+  { field: "fulladdress", headerName: "Address", width: 250 },
   { field: "ownerid", headerName: "Owner ID", width: 150 },
   { field: "ownername", headerName: "Owner Name", width: 150 },
   { field: "tenantid", headerName: "Tenant ID", width: 150 },
@@ -18,7 +19,6 @@ export const getColumns = (setOpen, setEditingRowId, setEditingRowData) => [
     headerName: "Manager Name",
     width: 150,
   },
-  { field: "fulladdress", headerName: "Address", width: 250 },
   {
     field: "propertyregisteredon",
     headerName: "Registered On",
@@ -35,19 +35,4 @@ export const getColumns = (setOpen, setEditingRowId, setEditingRowData) => [
     headerName: "Property Status",
     width: 180,
   },
-  // {
-  //   field: "options",
-  //   headerName: "Options",
-  //   width: 350,
-  //   sortable: false,
-  //   filterable: false,
-  //   renderCell: (params) => (
-  //     <PropertyRowOptions
-  //       row={params.row}
-  //       setOpen={setOpen}
-  //       setEditingRowId={setEditingRowId}
-  //       setEditingRowData={setEditingRowData}
-  //     />
-  //   ),
-  // },
 ];
